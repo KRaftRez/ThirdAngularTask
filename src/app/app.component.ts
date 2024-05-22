513bf75d05a4ebe1e3b85d9c4c20d1b0ac44b32c
Anastasis Prevenda <tehapcraftakosgr@gmail.com>
2024-05-22 13:28:47 +0300

    thirstangluartask

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { interval } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule,
+    ReactiveFormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'third-angular-project';
+export class AppComponent implements OnInit {
+
+  
+  folderform :FormGroup
+  listData: any;
+submited=false;
+
+
+  constructor(private fb:FormBuilder){
+
+this.listData=[];
+
+    this.folderform=this.fb.group({
+      ftitle :['', Validators.minLength(10)],
+      fcomment :['', Validators.minLength(7)]
+    })
+  }
+
+
+  public addItem(): void{
+    this.listData.push(this.folderform.value);
+    this.folderform.reset();
+  }
+
+
+
+
+removeItem(element: any){
+  this.listData.forEach((value: any, index: any)=>{
+    if(value == element)
+      this.listData.splice(index,1);
+  });
+
+  
+}
+
+editItem(element: any){
+  this.listData.forEach((value: any, index: any)=>{
+    if(value == element)
+      this.folderform.enable();
+  });
+
+  
+}
+
+
+
+  ngOnInit(){
+
+  }
 }
